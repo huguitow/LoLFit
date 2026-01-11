71e3edc702fd9f7f803caa762a08af60ea49dd3a
Hugo DESJARDINS <hdesj84@gmail.com>
2026-01-11 18:37:00 +0100

    Correction Repo

--- backend/src/main/java/com/lolfit/backend/skin/WardrobeRepository.java
+++ backend/src/main/java/com/lolfit/backend/skin/WardrobeRepository.java
@@ -2,10 +2,9 @@ package com.lolfit.backend.skin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import java.util.Optional;
 
 public interface WardrobeRepository extends JpaRepository<Wardrobe, Long> {
     List<Wardrobe> findByUserId(Long userId);
 
-    public boolean existsByUserIdAndSkinId(Long userId, Long skinId);
+    boolean existsByUserIdAndSkinId(Long userId, Long skinId);
 }
